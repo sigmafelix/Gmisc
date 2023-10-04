bee15a0caac6bd26453bef201b8bf7fe9276e4c8
Insang Song <sigmafelix@hotmail.com>
2023-10-04 13:57:00 -0400

    Update Rayshader_Asahi_1987_091721.r
    - maptools deprecation: geodata is used instead
    - Country code of geodata::world() outputs does not comply to ISO-3. Needs to be fixed later

--- Rayshader_Asahi_1987_091721.r
+++ Rayshader_Asahi_1987_091721.r
@@ -1,29 +1,30 @@
 ## Asahi Shimbun 01/01/1987 reproduction
 options(repos = 'https://ftp.osuosl.org/pub/cran/')
-library(pacman)
-pkgs = c('rayshader', 'ggplot2', 'tidyverse', 'maptools', 'sf', 'lazysf', 'data.table', 'rmapshaper')
+remotes::install_github("rspatial/geodata")
+pkgs = c('rayshader', 'ggplot2', 'tidyverse', 'geodata', 'sf', 'lazysf', 'data.table', 'rmapshaper')
 lapply(pkgs, library, character.only = TRUE)
 sf_use_s2(FALSE)
 
-rpath = '/home/felix/OneDrive/R_Study/Rayshader/'
+rpath = '.'
 
-data(wrld_simpl)
-fert = data.table::fread(str_c(rpath, 'Worldbank_Fertility.csv'), skip =4, header = TRUE)
+fert = data.table::fread('Worldbank_Fertility.csv', skip =4, header = TRUE)
 
-world = wrld_simpl %>%
+wrld_simpl = geodata::world(path = rpath)
+world0 = wrld_simpl %>%
     st_as_sf %>%
     st_cast('POLYGON') %>%
+    rename(ISO3 = GID_0) %>%
     mutate(AREA = st_area(geometry)) %>%
-    group_by(ISO3) %>%
+    group_by(ISO3, NAME_0) %>%
     arrange(ISO3, -AREA) %>%
     filter(rank(AREA) >= n()-1) %>%
     summarize(N = n()) %>%
     ungroup
 
 
-world_fert = world %>%
+world_fert = world0 %>%
     st_as_sf %>%
-    left_join(fert, by = c('ISO3' = 'Country Code'))
+    left_join(fert, by = c('NAME_0' = 'Country Name'))
 
 colnames(world_fert)[7:67] = str_c('Y', 1960:2020)
 
